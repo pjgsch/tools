ca8e332d7bebc329eceaec4e8c8838a071992725
pjgsch <100465642+pjgsch@users.noreply.github.com>
2022-03-02 12:17:06 +0100

    Add files via upload

---
+++ tpl/index.php
@@ -0,0 +1,31 @@
+<HTML><TITLE>readdir</TITLE></HEAD>
+
+<BODY BGCOLOR="#FFFFFF">
+<!--
+<FONT FACE="Arial,Helvetica">
+-->
+<H2>Download Page</H2>
+ 
+The files downloadable from this page are:
+<UL>
+<?php
+    $handle=opendir('.');
+    while ($file = readdir($handle)) 
+    {
+       if (($file != ".") && 
+           ($file != "..") && 
+           ($file != "index.php") &&
+           ($file != "index.html") &&
+           ($file != "default.htm"))
+       {
+          echo "<LI> <A HREF=\"$file\">$file</A></LI>\n";
+       }
+    }
+    closedir($handle);
+?>
+</UL>
+
+<!--
+</FONT>
+-->
+</BODY></HTML>
